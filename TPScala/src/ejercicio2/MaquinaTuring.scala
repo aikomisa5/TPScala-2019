e0c3de8b4bbbc0f3e02fedf4c80c17e198f814e0
mlucerocorrea <mluce@LAPTOP-CRAKFI9L>
2019-06-10 15:53:16 -0300

    cambios menores

--- TPScala/src/ejercicio2/MaquinaTuring.scala
+++ TPScala/src/ejercicio2/MaquinaTuring.scala
@@ -1,32 +1,23 @@
 package ejercicio2
 
-/*
- * Programar una maquina de Turing determinıstica. Para ello, implementar una clase
- * final class MaquinaTuring(transiciones: Map[(Estado, char), (Estado, char, Direccion)])
- * que tenga una funcion def procesar(inicio: (Estado, Cinta)): (Estado, Cinta)
- * que lance el proceso de la maquina y devuelva el estado final de la maquina y la cinta.
- * Estado, Cinta, Direccion deben ser clases definidas por ustedes que representen esa idea en la maquina de Turing.
- * La maquina puede construirse en memoria, como parte del codigo de casos de test, no esnecesario leer un archivo.
- */
   final class MaquinaTuring(transiciones: Map[(Estado, Char), (Estado, Char, Direccion)]) {
 
     // lance el proceso de la maquina y devuelva el estado final de la maquina y la cinta.
     // unidad lectora que lee y escribe en la cinta y se mueve a la izquierda o derecha
     def procesar(inicio: (Estado, Cinta)): (Estado, Cinta) = {
-      // encargado de escribir símbolos en la cinta y mover la cinta a la izquierda o a la derecha (siempre una celda a la vez)
-
       val actual= inicio
-      def cabezal(): Unit = {
-        if(!Estado.esEstadoFinal){
-          if(Direccion.direccion.equals("D"){ //si la direccion es derecha
-            Cinta.cinta= Nil :: Estado
-            // moverse
-            cabezal()
+    	// encargado de escribir símbolos en la cinta y mover la cinta a la izquierda o a la derecha (siempre una celda a la vez)
+      def cabezal(actual: (Estado, Char)): Unit = {
+        if(!Estado.esEstadoFinal){ //entra al loop siempre que no sea un estado final
+          if(Direccion.direccion.equals("D")){ //si la direccion es derecha
+            Cinta.cinta= Nil :: Estado //agregar el estado a la derecha
+            // moverse (modifica el actual)
+            cabezal(actual)
           }
           else { //direccion izq
-            Cinta.cinta= Estado :: Nil
-            moverse
-            cabezal()
+            Cinta.cinta= Estado :: Nil //agregar el estado a la izq
+            //moverse (modifica el actual)
+            cabezal(actual)
           }
         }
         else actual
