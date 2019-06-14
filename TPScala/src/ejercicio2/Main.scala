b0da5f74206f6d9bf645caa20ef8e8af3d1d0779
mlucerocorrea <mluce@LAPTOP-CRAKFI9L>
2019-06-14 10:57:28 -0300

    se hace un new MaquinaTuring en el main

--- TPScala/src/ejercicio2/Main.scala
+++ TPScala/src/ejercicio2/Main.scala
@@ -2,10 +2,12 @@ package ejercicio2
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val cinta= Cinta(List('a','a','a','a'))
+    val cinta = Cinta(List('a', 'a', 'a', 'a'))
     printf(cinta.cinta.head.toString())
-    val estadosFinales= List(Estado("X"),Estado("Z"))
-    
-    val m= MaquinaTuring((Estado("A"), 'b'), (Estado("A"), 'b', Direccion("D"))) 
+    val estadosFinales = List(Estado("X"), Estado("Z"))
+
+    val m = new MaquinaTuring(Map((Estado("A"), 'b') -> (Estado("A"), 'b', Direccion("D"))))
+
+    //    m.procesar(inicio)
   }
 }
--- TPScala/src/ejercicio2/MaquinaTuring.scala
+++ TPScala/src/ejercicio2/MaquinaTuring.scala
@@ -2,22 +2,33 @@ package ejercicio2
 
 final class MaquinaTuring(transiciones: Map[(Estado, Char), (Estado, Char, Direccion)]) {
 
-  def procesar(inicio: (Estado, Cinta)): (Estado, Cinta) = {
-    //      def cabezal(actual: (Estado, Char)): Unit = {
-    //        if(!Estado.esEstadoFinal){ //entra al loop siempre que no sea un estado final
-    //          if(Direccion.direccion.equals("D")){ //si la direccion es derecha
-    //            Cinta.cinta lista= Nil :: Estado //agregar el estado a la derecha
-    //            // moverse (modifica el actual)
-    //            cabezal(actual, lista)
-    //          }
-    //          else { //direccion izq
-    //            Cinta.cinta= Estado :: Nil //agregar el estado a la izq
-    //            //moverse (modifica el actual)
-    //            cabezal(actual, lista)
-    //          }
-    //        }
-    //        else actual
-    //      }
+  def procesar(inicio: (Estado, Cinta), l: List[(Estado)]): (Estado, Cinta) = {
+    val nuevacinta = inicio._2
+    def cabezal(actual: (Estado, Char)): Unit = {
+      def estadosFinales(actual: (Estado, Char), l: List[(Estado)]): Unit = { // para reccorrer los estados finales
+        if (!l.isEmpty) {
+          if (!actual._1.equals(l.head)) { // si no es un estado final
+            transiciones.keys
+              
+            
+          }
+          estadosFinales(actual, l.tail)
+        }
+      }
+      //        if(!Estado.esEstadoFinal){ //entra al loop siempre que no sea un estado final
+      //          if(Direccion.direccion.equals("D")){ //si la direccion es derecha
+      //            Cinta.cinta lista= Nil :: Estado //agregar el estado a la derecha
+      //            // moverse (modifica el actual)
+      //            cabezal(actual, lista)
+      //          }
+      //          else { //direccion izq
+      //            Cinta.cinta= Estado :: Nil //agregar el estado a la izq
+      //            //moverse (modifica el actual)
+      //            cabezal(actual, lista)
+      //          }
+      //        }
+      //        else actual
+    }
     inicio
   }
 
