e1a4e92b574875a03289759f37f50d9f0f72ba30
mlucerocorrea <mluce@LAPTOP-CRAKFI9L>
2019-06-15 21:26:53 -0300

    se hace la funcion recorrerString que recorre el alfabeto del imput

--- TPScala/src/ejercicio1/Main.scala
+++ TPScala/src/ejercicio1/Main.scala
@@ -7,23 +7,23 @@ object Main {
 
     try {
       val lines = Source.fromFile("automata.txt").getLines.toList
-      imprimir(lines)
+      imprimirAutomata(lines)
 
       val alfabeto = obtenerAlfabeto(lines)
-      println(alfabeto.foreach((c: String) => println("Elemento del alfabeto: " + c)))
-      
+      recorrerString(alfabeto)
+
       val cantidadEstado = obtenerCantidadEstados(lines)
       println("La cantidad de estados es: " + cantidadEstado)
-      
+
       val estadosFinales = obtenerEstadosFinales(lines)
       println(estadosFinales.foreach((c: String) => println("Estado final: " + c)))
-      
+
       procesar("Fifito")
-      
+
       pruebaSplit()
-      
+
       val tuplas = obtenerTransicionesEnTuplas(lines)
-      
+
       tuplas.foreach(f => println("El valor del elemento es: " + f))
 
     } catch {
@@ -32,11 +32,18 @@ object Main {
 
     }
 
+    def recorrerString(w: Array[String], puntero: Int = 0): Unit = {
+      if (w.size > 0) {
+        println("elemento del string: " + w.head)
+        recorrerString(w.tail, puntero + 1)
+      }
+    }
+
     @annotation.tailrec
-    def imprimir[A](l: List[A]): Unit = {
+    def imprimirAutomata[A](l: List[A]): Unit = {
       if (!l.isEmpty) {
         println(l.head)
-        imprimir(l.tail)
+        imprimirAutomata(l.tail)
       }
     }
 
@@ -47,109 +54,104 @@ object Main {
       //Retorno alfa
       alfa
     }
-    
+
     @annotation.tailrec
     def obtenerEstadosFinales(lista: List[String], puntero: Int = 0): Array[String] = {
       if (puntero == 2)
         lista.head.split(", ")
       else
-        obtenerEstadosFinales(lista.tail, puntero+1)
+        obtenerEstadosFinales(lista.tail, puntero + 1)
     }
-    
+
     @annotation.tailrec
     def obtenerCantidadEstados(lista: List[String], puntero: Int = 0): String = {
       if (puntero == 1)
         lista.head
       else
-        obtenerCantidadEstados(lista.tail, puntero+1)
+        obtenerCantidadEstados(lista.tail, puntero + 1)
     }
-    
-      
-    def obtenerTransicionesEnTuplas(transiciones: List[String], tuplas: List[(String,String,String,Boolean)] = List[(String,String,String,Boolean)]() , puntero: Int = 0): List[(String,String,String,Boolean)] = {
+
+    def obtenerTransicionesEnTuplas(transiciones: List[String], tuplas: List[(String, String, String, Boolean)] = List[(String, String, String, Boolean)](), puntero: Int = 0): List[(String, String, String, Boolean)] = {
       println("Paso x aca 1")
       println("Valor del puntero: " + puntero)
       val text = "1, a -> 3"
       val words = text.split("[,-]>? ?")
-      
-      if (puntero >= 3 && !transiciones.isEmpty){
+
+      if (puntero >= 3 && !transiciones.isEmpty) {
         println("Paso x aca 2")
         val array = transiciones.head.split(" ?[,-]>? ?")
         println("Paso x aca a")
         array.foreach(f => println("Elemento: " + f))
-        val tuplasAux = tuplas:+ ((array(0),array(1),array(2),false))
+        val tuplasAux = tuplas :+ ((array(0), array(1), array(2), false))
         println("Paso x aca b")
-        obtenerTransicionesEnTuplas(transiciones.tail, tuplasAux, puntero+1)
-        
-      }
-      else if (puntero >= 3 && transiciones.isEmpty){
+        obtenerTransicionesEnTuplas(transiciones.tail, tuplasAux, puntero + 1)
+
+      } else if (puntero >= 3 && transiciones.isEmpty) {
         println("Paso x aca 3")
         tuplas
-      }
-      else{
+      } else {
         println("Paso x aca 4")
-        obtenerTransicionesEnTuplas(transiciones.tail, tuplas, puntero+1)
+        obtenerTransicionesEnTuplas(transiciones.tail, tuplas, puntero + 1)
       }
-      
+
       //val a : List[(String,String,String,Boolean)] = List()
-      //val b = a:+(("x","y","z",true))     
-      
-    } 
-            
+      //val b = a:+(("x","y","z",true))
+
+    }
+
     def pruebaSplit(): Unit = {
-                     
+
       //val text = "s: saturday, sunday, solar, selfie";
       //val words = text.split("[:,] ");
-      
-      val text = "1, a -> 3";  
+
+      val text = "1, a -> 3";
       val words = text.split("[,-]>? ?")
-      
-      words.foreach(f => println ("Elemento del array: " + f))
-      
+
+      words.foreach(f => println("Elemento del array: " + f))
+
     }
-        
+
     def procesar(w: String): Boolean = {
       val info = (5, "Juan", false)
       val info2 = info.copy(info._1, info._2, info._3)
-      println ("Info 2: " + info2)
-      true  
+      println("Info 2: " + info2)
+      true
     }
-    
+
     def proceso(cadena: String): Boolean = {
       val lines = Source.fromFile("automata.txt").getLines.toList
       val tuplas = obtenerTransicionesEnTuplas(lines)
-      
-      //procesoRecursivo("1", cadena, tuplas)  
+
+      //procesoRecursivo("1", cadena, tuplas)
       true
     }
-    
+
     /*
     def procesoRecursivo(estadoActual : String, cadena : String, transicionesTuplas : List[(String,String,String,Boolean)]) : Boolean = {
       val b = false
-      
+
       cadena.foreach(f=>{
         b = b + false
       })
-      
-      true 
+
+      true
     }
-    
+
     */
-    
-    def seEncuentraEnAlgunaTransicion(c : Char, estadoActual : String): Boolean = {
+
+    def seEncuentraEnAlgunaTransicion(c: Char, estadoActual: String): Boolean = {
       val lines = Source.fromFile("automata.txt").getLines.toList
       val tuplas = obtenerTransicionesEnTuplas(lines)
-      
+
       tuplas.foreach(f => {
-        if (f._2 == c && f._1 == estadoActual){
+        if (f._2 == c && f._1 == estadoActual) {
           true
         }
-          
+
       })
-      
+
       false
     }
-    
-    
 
     /*
     def verificarInput (input : String): Boolean = {
