33c07d0691fe18bb786f95ce93bc707b3eddaee3
mluce <mluce@DESKTOP-GDDUEL1>
2019-06-18 01:13:08 -0300

    la maquinadeturing se detiene cuando encuentra un estado final

--- TPScala/src/ejercicio2/Main.scala
+++ TPScala/src/ejercicio2/Main.scala
@@ -5,10 +5,10 @@ object Main {
     val cinta = Cinta(List('a', 'a', 'a', 'a'))
     val estadosFinales = List(Estado("X"), Estado("Z"))
 
-    val m = new MaquinaTuring(Map((Estado("A"), 'a') -> (Estado("B"), 'b', Direccion("D"))))
+    val m = new MaquinaTuring(Map((Estado("A"), 'a') -> (Estado("Z"), 'b', Direccion("D"))))
 
     val tupla = (Estado("A"), cinta)
-    val procesado = m.procesar(tupla)
+    val procesado = m.procesar(tupla, estadosFinales)
 
     println("procesado -> " + procesado)
   }
--- TPScala/src/ejercicio2/MaquinaTuring.scala
+++ TPScala/src/ejercicio2/MaquinaTuring.scala
@@ -4,14 +4,16 @@ final class MaquinaTuring(transiciones: Map[(Estado, Char), (Estado, Char, Direc
 
   //  val t = transiciones(Estado("A"), 'a')
 
-  def procesar(inicio: (Estado, Cinta)): (Estado, Cinta) = {
+  def procesar(inicio: (Estado, Cinta), estadosFinales: List[(Estado)]): (Estado, Cinta) = {
     val cantElementosCinta = numElems(inicio._2.cinta.toList)
-    cabezal((inicio._1, Cinta(List())), inicio._2, transiciones(Estado("A"), 'a'))
+    cabezal((inicio._1, Cinta(List())), inicio._2, transiciones(Estado("A"), 'a'), estadosFinales)
   }
 
   @annotation.tailrec
-  def cabezal(actual: (Estado, Cinta), cintaOriginal: Cinta, t: (Estado, Char, Direccion)): (Estado, Cinta) = {
-    if (actual._1.e == new Estado("F"))
+  def cabezal(actual: (Estado, Cinta), cintaOriginal: Cinta, t: (Estado, Char, Direccion), estadosFinales: List[(Estado)], intEst: Int = 0): (Estado, Cinta) = {
+    if (estadosFinales.size == intEst)
+      return actual
+    if (actual._1 == estadosFinales(intEst))
       return actual
     if (numElems(actual._2.cinta.toList) == numElems(cintaOriginal.cinta.toList))
       return actual
@@ -19,7 +21,7 @@ final class MaquinaTuring(transiciones: Map[(Estado, Char), (Estado, Char, Direc
       case Direccion("D") => Cinta(actual._2.cinta ::: List(t._2))
       case Direccion("I") => Cinta(t._2 :: actual._2.cinta)
     }
-    cabezal((t._1, izqOrDer(t._3)), cintaOriginal, t)
+    cabezal((t._1, izqOrDer(t._3)), cintaOriginal, t, estadosFinales, intEst + 1)
   }
 
   def numElems(l: List[Char]): Int =
